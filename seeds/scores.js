b9668361a00505bd545d5c8f2b7da700a832a500
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-02-10 15:32:18 +1300

    Set up seeds for database

---
+++ seeds/scores.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('scores').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('scores').insert([
+        {id: 1, date: '2018-02-7', score: 232, typing_accuracy: 82.1, wave: 007, longest_streak: 32},
+        {id: 2, date: '2018-02-8', score: 267, typing_accuracy: 77.7, wave: 008, longest_streak: 27},
+        {id: 3, date: '2018-02-9', score: 274, typing_accuracy: 81, wave: 007, longest_streak: 54}
+      ]);
+    });
+};
