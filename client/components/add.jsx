ae62da929814f25e22c6c6e8784a0dad9f1a0858
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-02-12 06:49:03 +1300

    Set up add score form

--- client/components/add.jsx
+++ client/components/add.jsx
@@ -4,6 +4,14 @@ const Add = props => {
   return (
     <div>
       <h2>Add Score</h2>
+      <form id="add-score" method="POST">
+        <label>Date <input type="date" name="date" /></label>
+        <label>Score <input type="number" name="score" /></label>
+        <label>Typing Accuracy <input type="number" name="typing_accuracy" /></label>
+        <label>Wave <input type="number" name="wave" /></label>
+        <label>Longest Streak <input type="number" name="longest_streak" /></label>
+        <input type="submit" value="Add Score" />
+      </form>
     </div>
   )
 }
