d19ce87e9a13c30e1ab5cb391263c134c17bc900
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-03-17 20:37:08 +1300

    Set up auth routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const scoresRoutes = require('./routes/scores')
+const authRoutes = require('./routes/auth')
 
 const app = express()
 app.use(express.static('public'))
@@ -9,5 +10,6 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.use('/api/scores', scoresRoutes)
+app.use('/api/auth', authRoutes)
 
 module.exports = app
