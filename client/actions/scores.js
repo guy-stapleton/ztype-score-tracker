2f0ce2c81e69447987f15f7f2312b74b626afc92
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-03-22 16:15:38 +1300

    Set up addscore api request

--- client/actions/scores.js
+++ client/actions/scores.js
@@ -9,6 +9,28 @@ export const storeScores = scores => {
   }
 }
 
+export const storeScore = score => {
+  return {
+    type: 'ADD_SCORE_TO_SCORE',
+    score
+  }
+}
+
+
+export function addScore(score) {
+  return dispatch => {
+    request
+      .post(scoresApi)
+      .send(score)
+      .end((err, res) => {
+        if (err) {
+          return err.message
+        }
+        dispatch(storeScore(res.body))
+      })
+  }
+}
+
 export function getScores () {
   return dispatch => {
     request
