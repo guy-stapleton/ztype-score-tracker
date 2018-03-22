3695a14aa58bb7e79ff886ad677af8064ffc0b6b
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-03-22 16:17:45 +1300

    Set up addscore and find score db requests

--- server/db/scores.js
+++ server/db/scores.js
@@ -5,4 +5,24 @@ function getScores(testDb) {
   return db('scores').select()
 }
 
-module.exports = {getScores}
+function addScore(score, testDb) {
+  console.log(`addScore ran`)
+  const db = testDb || connection
+  return db('scores')
+    .insert({date: score.date,
+            score: score.score,
+            typing_accuracy: score.typing_accuracy,
+            wave: score.wave,
+            longest_streak: score.longest_streak})
+}
+
+function findScore(scoreId, testDb) {
+  const db = testDb || connection
+  return db('scores')
+    .where('id', scoreId)
+    .select()
+}
+
+module.exports = {getScores,
+                  findScore,
+                  addScore}
