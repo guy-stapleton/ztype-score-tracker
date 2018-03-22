48267e18dedf2bfc18ac70b6df4cd45101538f59
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-03-22 16:16:57 +1300

    Set up reducer to add score to store

--- client/reducers/scores.js
+++ client/reducers/scores.js
@@ -1,7 +1,10 @@
 function scores(state = [], action) {
+  let newState = [...state]
   switch(action.type) {
     case 'ADD_SCORES_TO_STORE':
     return action.scores
+    case 'ADD_SCORE_TO_SCORE':
+    return [...newState, action.score]
     default:
     return state
   }
