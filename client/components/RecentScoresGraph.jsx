cf1fe93dc93fa6717754aa690a2f6a277105f11f
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-03-23 18:25:46 +1300

    Set up scores to display based on data in store

--- client/components/RecentScoresGraph.jsx
+++ client/components/RecentScoresGraph.jsx
@@ -1,22 +1,52 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
+import {getScores} from '../actions/scores'
+
 class RecentScoresGraph extends React.Component {
   constructor(props) {
   super(props)
+
+  this.createGraph = this.createGraph.bind(this)
   }
-  componentDiDMount() {
+  componentDidMount() {
+    this.props.dispatch(getScores())
+  }
+
+
+  createGraph(scores) {
+    var data = scores.map((score, i) => {
+      return score.score
+    })
+
+    let maxValue = Math.max(...data)
+
+    console.log(maxValue)
+
+    var x = d3.scale.linear()
+      .domain([0, d3.max(data)])
+      .range([0, maxValue])
+
+
+    d3.select('.chart')
+      .selectAll('div')
+      .data(data)
+      .enter().append('div')
+      .attr('class', 'bar')
+      .style('width', function(d) {return x(d) + 'px'})
+      .style('height', function(d) {return 30 + 'px'})
+      .style('font-size', function(d) {return 1.5 + 'em'})
+      .style('padding-right', function(d) {return 0.5 + 'em'})
+      .style('padding-bottom', function(d) {return 0.5 + 'em'})
+      .style('margin-bottom', function(d) {return 0.5 + 'em'})
+      .text(function(d) {return d})
 
   }
-  shouldComponentUpdate() {
-    return false
-  }
+
   render() {
+    this.createGraph(this.props.scores)
     return (
-      <div id="graph">
-        {this.props.scores.map((score, i) => {
-          return <p key={i}>{score.score}</p>
-        })}
+      <div className="chart">
       </div>
     )
   }
