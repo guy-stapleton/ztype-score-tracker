f447f02eb15fc4132ca74d5ff75ebc0366a1dacb
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-03-25 21:24:29 +1300

    Updated waves field

--- seeds/scores.js
+++ seeds/scores.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('scores').insert([
-        {id: 1, date: '2018-02-7', score: 232, typing_accuracy: 82.1, wave: 007, longest_streak: 32},
-        {id: 2, date: '2018-02-8', score: 267, typing_accuracy: 77.7, wave: 008, longest_streak: 27},
-        {id: 3, date: '2018-02-9', score: 274, typing_accuracy: 81, wave: 007, longest_streak: 54}
+        {id: 1, date: '2018-02-7', score: 232, typing_accuracy: 82.1, wave: 7, longest_streak: 32},
+        {id: 2, date: '2018-02-8', score: 267, typing_accuracy: 77.7, wave: 8, longest_streak: 27},
+        {id: 3, date: '2018-02-9', score: 274, typing_accuracy: 81, wave: 7, longest_streak: 54}
       ]);
     });
 };
